31250e1ab9d535d6f7a117fe810163fb73dd22c6
Troysaved <troysatchell@gmail.com>
2023-11-27 16:11:53 -0500

    Firebase Sync higherorder component

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -9,20 +9,23 @@ import admin from "firebase-admin";
 let app;
 
 if (!admin.apps.length) {
-  console.log('Initializing app');
   app = admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
     }),
   });
-} else {
-  console.log('App already initialized');
-  app = admin.apps[0]; // Use the existing app
 }
 
-const adminDb = app.firestore();
-const adminAuth = app.auth();
+const adminDb = initFirestore({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
+});
+
+const adminAuth = admin.auth(app);
 
 export { adminDb, adminAuth };
